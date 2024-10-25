72b6ab15a4987a7fa41f3600e25d87da27156919
Sam Ford <samueljamesford@googlemail.com>
2024-10-25 18:51:28 +0100

    Adds Conditional styles to Directory Page

--- src/components/userDisplay.tsx
+++ src/components/userDisplay.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { UserType } from '@/types/User';
 import '../app/globals.css';
 
@@ -6,13 +7,21 @@ interface Props {user: UserType;}
 
 export const UserDisplay: React.FC<Props> = ({ user }) => {
 return (
-    <div>
-<Link 
-    href={`/users/${user.user_id}`}
->
-{user.first_name} {user.last_name}
-</Link>
+<Link className='user-display-card' href={`/users/${user.user_id}`}>
 
+<div className={`image-container ${user.in_building ? 'present' : 'absent'}`}>
+  <Image 
+    src={user.picture_url} 
+    alt={`${user.first_name} ${user.last_name}'s picture`} 
+    width={50} 
+    height={50} 
+  />
+</div>
+    <div>
+        {user.first_name} {user.last_name}<br/>
+        {user.job_title}
     </div>
+
+</Link>
 );
 };
